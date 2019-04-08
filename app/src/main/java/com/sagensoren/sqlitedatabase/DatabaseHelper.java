f726d804da36d970d9461c045447ec8f6e307213
SAGEN SAKAM BHIM SOREN <35399866+SSBsoren@users.noreply.github.com>
2019-04-08 20:31:33 +0530

    Done!!

--- app/src/main/java/com/sagensoren/sqlitedatabase/DatabaseHelper.java
+++ app/src/main/java/com/sagensoren/sqlitedatabase/DatabaseHelper.java
@@ -24,69 +24,71 @@ public class DatabaseHelper  extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS "
-        + TABLE_NAME+
-                "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
-                " NAME TEXT ," +
+                + TABLE_NAME+
+                " (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                " NAME TEXT," +
                 " EMAIL TEXT," +
                 " COURSE_COUNT INTEGER)");
-
     }
-
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(sqLiteDatabase);
-
     }
 
-    public boolean insertData(String name, String email,String courseCount){
+    public boolean insertData(String name, String email, String coursCount){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2,name);
-        contentValues.put(COL_3,email);
-        contentValues.put(COL_4,courseCount);
 
-        long result = db.insert(TABLE_NAME,null,contentValues);
-        if (result == -1 ){
-            return  false;
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, email);
+        contentValues.put(COL_4, coursCount);
 
-        }else {
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1){
+            return false;
+        } else {
             return true;
         }
+
+
     }
 
-    public boolean updateData(String id, String name,String email, String courseCount){
+    public boolean updateData(String id, String name, String email, String courseCount){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
 
-        contentValues.put(COL_1,name);
-        contentValues.put(COL_2,name);
-        contentValues.put(COL_3,email);
-        contentValues.put(COL_4,courseCount);
+        contentValues.put(COL_1, id);
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, email);
+        contentValues.put(COL_4, courseCount);
 
-        db.update(TABLE_NAME,contentValues,"ID=?", new String[]{id});
+        db.update(TABLE_NAME, contentValues, "ID=?", new String[]{id});
         return true;
-
     }
 
     public Cursor getData(String id){
         SQLiteDatabase db = this.getWritableDatabase();
         String query = "SELECT * FROM "+TABLE_NAME+" WHERE ID='"+id+"'";
-        Cursor cursor = db.rawQuery(query,null);
+        Cursor cursor = db.rawQuery(query, null);
+
         return cursor;
+
+
     }
 
     public Integer deleteData(String id){
         SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete(TABLE_NAME, "ID=?",new String[]{id});
+
+        return db.delete(TABLE_NAME, "ID=?", new String[]{id});
+
     }
 
     public Cursor getAllData(){
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM "+TABLE_NAME,null);
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TABLE_NAME, null);
         return cursor;
     }
-
 
 
 }
